721db6f979073cb4972c0b9a7ae869a9123a6775
DP39 <dpasqualm@gmail.com>
2024-09-14 11:28:19 +1200

    Update HigherLowerGame.py

--- HigherLowerGame/HigherLowerGame.py
+++ HigherLowerGame/HigherLowerGame.py
@@ -1,38 +1,48 @@
 from game_data import *
 import sys
+import random
 
-response = input("Welcome to Higher and Lower. Press a 'Y' to continue. ")
+# Confirm with user to continue.
+response = input("Welcome to Higher and Lower. Press a 'Y' to continue. Else exist. ")
 if response != "y" and response != "Y":
     sys.exit()
 
-index = 0
-for index in range(len(data)-1):
+# Go through each question in the list.
+question_count = 0
+continue_game = True
+first_set = random.choice(data)
+second_set = first_set
+while continue_game:
+    # Read information.
+    while first_set == second_set:
+        second_set = random.choice(data)
+
+    # Show the question.
     print("")
     print("Compare A: {0}, {1}, from {2}".
-          format(data[index].get("name"), data[index].get("description"),
-                 data[index].get("country")))
+          format(first_set.get("name"), first_set.get("description"),
+                 first_set.get("country")))
     print("Against B: {0}, {1}, from {2}".
-          format(data[index+1].get("name"), data[index+1].get("description"),
-                 data[index+1].get("country")))
+          format(second_set.get("name"), second_set.get("description"),
+                 second_set.get("country")))
 
+    # Get user's answer and continue till A or B is entered.
     get_input = True
-    continue_game = False
+    response = ""
     while get_input:
         response = input("Who has more followers? Please enter A or B. ")
-        if response == "a" or response == "A":
+        if response in ["a", "A", "b", "B"]:
             get_input = False
-            if data[index].get("follower_count") >= data[index+1].get("follower_count"):
-                continue_game = True
-        elif response == "b" or response == "B":
-            get_input = False
-            if data[index].get("follower_count") <= data[index + 1].get("follower_count"):
-                continue_game = True
 
-    if not continue_game:
+    # Check if user's answer is correct.
+    answer = "a" if first_set.get("follower_count") >= second_set.get("follower_count") else "b"
+    if response.lower() == answer:
+        continue_game = True
+        question_count = question_count + 1
+        first_set = second_set
+    else:
         print("Incorrect answer.")
-        break
+        continue_game = False
 
-if index == len(data)-2:
-    print("Great.")
-
-print("Your score is {0}".format(index))
+# Print the score.
+print("Your score is {0}".format(question_count))
